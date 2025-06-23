ac44db0c1c6bfeaca411b9f7690119f8a8ab6dba
premod_1 <premod@pramixit.com>
2025-06-23 20:13:42 +0530

    feat: Enhance gallery functionality; add dummy categories for images and update route naming for tours

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [FrontendController::class, 'home'])->name('home');
 Route::get('/about', [FrontendController::class, 'about'])->name('about');
-Route::get('/service', [FrontendController::class, 'service'])->name('service');
+Route::get('/tours', [FrontendController::class, 'service'])->name('service');
 Route::get('/blog', [FrontendController::class, 'blog'])->name('blog');
 Route::post('/blog/loadMore', [FrontendController::class, 'loardMoreBlogs']);
 Route::get('/gallery', [FrontendController::class, 'gallery'])->name('gallery');
@@ -21,7 +21,7 @@ Route::post('/subscribe', [SubscribeController::class, 'subscribe']);
 Route::post('/contactFormSubmit', [ContactController::class, 'submitContactForm']);
 
 // Tour Package Routes
-Route::get('/tours', [TourPackageController::class, 'index'])->name('tours');
+Route::get('/services', [TourPackageController::class, 'index'])->name('tours');
 Route::get('/tours/tailor-made', [TourPackageController::class, 'tailorMade'])->name('tours.tailor-made');
 Route::get('/tours/round-tour', [TourPackageController::class, 'roundTour'])->name('tours.round-tour');
 Route::get('/tours/tpd', [TourPackageController::class, 'tpd'])->name('tours.tpd'); // Template view for TPD showcase
